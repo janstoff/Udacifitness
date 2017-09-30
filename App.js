cc58533b9972d9883f0c28f10ec833408c2f53a1
Jan Steinhoff <jan.steinhoff@gmail.com>
2017-09-30 12:11:08 +0200

    implement store and debug

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import store from './Store'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={store}>
         <View style={{flex: 1}}>
           <History />
         </View>
