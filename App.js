44f5ed054f9704a0145c9aa5f09050b789bf6eff
Jan Steinhoff <jan.steinhoff@gmail.com>
2017-10-02 12:29:18 +0200

    add live tab and base functionality

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { TabNavigator, StackNavigator } from 'react-navigation'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 function CustomStatusBar({ backgroundColor, ...props }) {
 	return (
@@ -40,7 +41,14 @@ const Tabs = TabNavigator(
 					<FontAwesome name="plus-square" size={30} color={tintColor} />
 				)
 			}
-		}
+		},
+    Live: {
+      screen: Live,
+      navigationOptions: {
+        tabBarLabel: 'Live',
+        tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+      }
+    }
 	},
 	//style via 2nd argument to TabNavigator()
 	{
